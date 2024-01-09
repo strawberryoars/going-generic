7c1a00e2abcc9ccce015440b652ab0b774b41f63
strawberryoars <devincdennis+1@gmail.com>
2024-01-08 21:38:00 -1000

    wip

--- apps/resources-api/main.go
+++ apps/resources-api/main.go
@@ -3,28 +3,49 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"fmt"
+	"log"
 	"net/http"
+
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
-clientOptions := options.Client().ApplyURI("mongodb+srv://username:password@clusterurl/test?retryWrites=true&w=majority")
-client, err := mongo.Connect(context.TODO(), clientOptions)
-if err != nil {
-   panic(err)
+var client *mongo.Client
+
+func init() {
+	var err error
+	clientOptions := options.Client().ApplyURI("mongodb+srv://")
+
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		panic(err)
+	}
+	err = client.Ping(context.TODO(), readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+type BlogPost struct {
+	ID    primitive.ObjectID `bson:"_id"`
+	Title string             `bson:"title"`
+	Body  string             `bson:"body"`
+	Type  string             `bson:"type"`
 }
 
 func handleRequest(w http.ResponseWriter, r *http.Request) {
 	// Get a handle for your collection
-	collection := client.Database("test").Collection("test")
+	collection := client.Database("resources").Collection("blogs")
 
 	// Define the filter
-	filter := bson.D{{"type", "Oolong"}}
+	filter := bson.D{{Key: "type", Value: "article"}}
 
 	// Execute the query
-	var results []Tea
+	var results []BlogPost
 	cursor, err := collection.Find(context.TODO(), filter)
 	if err != nil {
 		panic(err)
@@ -33,21 +54,13 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 
-	// Convert results to JSON and write to the response
+	// Convert results to JSON and write to the r esponse
 	res, _ := json.Marshal(results)
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(res)
 }
 
 func main() {
-http.HandleFunc("/query", handleRequest)
-http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/query", handleRequest)
+	http.ListenAndServe(":8080", nil)
 }
- 
-// func main() {
-// 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-// 		fmt.Fprint(w, "Hello World!")
-// 	})
-
-// 	http.ListenAndServe(":8080", nil) 
-// }
