7a2764617b5c757614c9ed4b8da07ebd4e3d5345
strawberryoars <devincdennis+1@gmail.com>
2024-01-11 13:52:10 -1000

    filtering on list endpoint

--- apps/resources-api/controllers/list.go
+++ apps/resources-api/controllers/list.go
@@ -6,18 +6,48 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 
 	"github.com/strawberryoars/going-generic/apps/resources-api/clients"
 	"go.mongodb.org/mongo-driver/bson"
 )
 
+// Generic List Resources endpoint - /resources/:resourceName?filter={}
+// URI components should be encoded like so:
+//
+//	let jsonString = '{"type":"article"}';
+//	let encodedJson = encodeURIComponent(jsonString);
+//	let url = `http://localhost:8080/resources/blog?filter=${encodedJson}`;
+//
+// For Example:
+//
+//	http://localhost:8080/resources/blogs?filter=%7B%22type%22%3A%22article%22%7D
 func ListHandler(w http.ResponseWriter, r *http.Request, resourceName string) {
 	logMessage := fmt.Sprintf("ListHandler - GET request /resources/%s", resourceName)
 	log.Println(logMessage)
 
 	collection := clients.Client.Database("resources").Collection(resourceName)
 
-	filter := bson.D{{Key: "type", Value: "article"}}
+	var filter bson.D
+	filterParam := r.URL.Query().Get("filter")
+
+	if filterParam != "" {
+		decodedFilterParam, err := url.QueryUnescape(filterParam)
+		if err != nil {
+			log.Println("Failed to decode filter query parameter: ", err)
+			http.Error(w, "Invalid filter query parameter", http.StatusBadRequest)
+			return
+		}
+
+		err = bson.UnmarshalExtJSON([]byte(decodedFilterParam), true, &filter)
+		if err != nil {
+			log.Println("Failed to parse filter query parameter:", err)
+			http.Error(w, "Invalid filter query parameter", http.StatusBadRequest)
+			return
+		}
+	} else {
+		filter = bson.D{}
+	}
 
 	var results []map[string]interface{}
 	cursor, err := collection.Find(context.TODO(), filter)
