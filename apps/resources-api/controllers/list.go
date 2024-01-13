8661e570d00481845d38f57503e8557b29f7251c
strawberryoars <devincdennis+1@gmail.com>
2024-01-13 13:28:05 -1000

    pagination params workings - list resources endpoint

--- apps/resources-api/controllers/list.go
+++ apps/resources-api/controllers/list.go
@@ -5,8 +5,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math"
 	"net/http"
 	"net/url"
+	"strconv"
 
 	"github.com/strawberryoars/going-generic/apps/resources-api/clients"
 	"go.mongodb.org/mongo-driver/bson"
@@ -14,7 +16,7 @@ import (
 )
 
 // Generic List Resources endpoint - /resources/:resourceName?filter={}&sort={}
-// URI components should be encoded like so:
+// URI components for filtering and sorting should be encoded like so:
 //
 //	let jsonString = '{"type":"article"}';
 //	let encodedJson = encodeURIComponent(jsonString);
@@ -24,19 +26,18 @@ import (
 // sort order: 1 for ascendeing and -1 for descending
 // sort = { key: order }
 //
-// TODO:
-// pagination params
-//
 // For Example:
 //
-//	curl -X GET http://localhost:8080/resources/blogs?filter=%7B%22type%22%3A%22article%22%7D
-//	curl -X GET http://localhost:8080/resources/blogs?sort=%7B%22type%22%3A1%7D
+//	curl -X GET 'http://localhost:8080/resources/blogs?page=1&pageSize=1
+//	curl -X GET 'http://localhost:8080/resources/blogs?filter=%7B%22type%22%3A%22article%22%7D&page=1&pageSize=10'
+//	curl -X GET 'http://localhost:8080/resources/blogs?sort=%7B%22type%22%3A1%7D&page=1&pageSize=10
 func ListHandler(w http.ResponseWriter, r *http.Request, resourceName string) {
 	logMessage := fmt.Sprintf("ListHandler - GET request /resources/%s", resourceName)
 	log.Println(logMessage)
 
 	collection := clients.Client.Database("resources").Collection(resourceName)
 
+	// Parse Filter Query Parameter
 	var filter bson.D
 	filterParam := r.URL.Query().Get("filter")
 
@@ -58,6 +59,7 @@ func ListHandler(w http.ResponseWriter, r *http.Request, resourceName string) {
 		filter = bson.D{}
 	}
 
+	// Parse Sort Query Parameter
 	var sort bson.D
 	sortParam := r.URL.Query().Get("sort")
 
@@ -79,7 +81,33 @@ func ListHandler(w http.ResponseWriter, r *http.Request, resourceName string) {
 		sort = bson.D{}
 	}
 
-	opts := options.Find().SetSort(sort)
+	// Parse Pagination Query Parameter
+	pageParam := r.URL.Query().Get("page")
+	pageSizeParam := r.URL.Query().Get("pageSize")
+
+	pageNumber, err := strconv.Atoi(pageParam)
+	if err != nil || pageNumber < 1 {
+		log.Println("Invalid or missing page number")
+		http.Error(w, "Invalid or missing page number", http.StatusBadRequest)
+		return
+	}
+
+	pageSize, err := strconv.Atoi(pageSizeParam)
+	if err != nil || pageSize < 1 {
+		log.Println("Invalid or missing page size")
+		http.Error(w, "Invalid or missing page size", http.StatusBadRequest)
+		return
+	}
+
+	totalCount, err := collection.CountDocuments(context.TODO(), filter)
+	if err != nil {
+		panic(err)
+	}
+	totalNumberOfPages := int(math.Ceil(float64(totalCount) / float64(pageSize)))
+	skip := (pageNumber - 1) * pageSize
+	opts := options.Find().SetSort(sort).SetSkip(int64(skip)).SetLimit(int64(pageSize))
+
+	// opts := options.Find().SetSort(sort)
 	var results []map[string]interface{}
 	cursor, err := collection.Find(context.TODO(), filter, opts)
 	if err != nil {
@@ -89,7 +117,16 @@ func ListHandler(w http.ResponseWriter, r *http.Request, resourceName string) {
 		panic(err)
 	}
 
-	res, _ := json.Marshal(results)
+	response := map[string]interface{}{
+		"results": results,
+		"pagination": map[string]interface{}{
+			"total": totalCount,
+			"page":  pageNumber,
+			"pages": totalNumberOfPages,
+		},
+	}
+
+	res, _ := json.Marshal(response)
 
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(res)
