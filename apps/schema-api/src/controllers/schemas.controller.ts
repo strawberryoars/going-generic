69f109d41fd364305635e1669133b183d49d0560
strawberryoars <devincdennis+1@gmail.com>
2024-01-14 15:22:35 -1000

    list all schemas endpoint

--- apps/schema-api/src/controllers/schemas.controller.ts
+++ apps/schema-api/src/controllers/schemas.controller.ts
@@ -1,9 +1,32 @@
 import { Elysia } from 'elysia';
-import { existsSync } from 'fs';
+import { existsSync, readdirSync } from 'fs';
+import { join } from 'path';
 
+// schemas endpoint
 export const schemas = new Elysia()
-  .group('/schemas', (app) =>
+  .group('/schemas', (app) => 
     app
+      // serves all schemas
+      .get('/', async (context) => {
+        const dirPath: string = `${import.meta.dir.split('/').slice(0, -2).join('/')}/schemas`;
+        console.log('dirPath', dirPath); 
+
+        if (!existsSync(dirPath)) {
+          return new Response("Directory not found", { status: 404 });
+        }
+
+        const files = readdirSync(dirPath);
+        const schemas = await Promise.all(files.map(async (file) => {
+          const filePath = join(dirPath, file);
+          const fileData = await Bun.file(filePath).json();
+          return fileData;
+        }));
+
+        return new Response(JSON.stringify(schemas), {
+          headers: { 'Content-Type': 'application/json' },
+        });
+      })
+      // serves requested schema
       .get('/:schema', async (context) => {
         const path: string = context.path;
         const filePath: string = `${import.meta.dir.split('/').slice(0, -2).join('/')}/${path.replace(/^\/|\/$/g, '')}.json`;
