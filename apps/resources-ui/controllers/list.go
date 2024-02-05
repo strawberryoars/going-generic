386230851b8e9fb4af4b6b0e665d6df7a4a796f3
strawberryoars <devincdennis+1@gmail.com>
2024-02-04 15:58:26 -1000

    wip - viewport changes coming

--- apps/resources-ui/controllers/list.go
+++ apps/resources-ui/controllers/list.go
@@ -3,11 +3,18 @@ package controllers
 import (
 	"encoding/json"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 func ListHandler(w http.ResponseWriter, r *http.Request, resourceName string) {
-	resp, err := http.Get("http://localhost:8080/resources/test?page=1&pageSize=2")
+	// resp, err := http.Get("http://localhost:8080/resources/test?page=1&pageSize=2")
+	parts := strings.Split(r.URL.Path, "/")
+	baseUrl := "http://localhost:8080/resources/" + parts[2]
+	fullUrl := baseUrl + "?" + r.URL.RawQuery
+	resp, err := http.Get(fullUrl)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -27,13 +34,29 @@ func ListHandler(w http.ResponseWriter, r *http.Request, resourceName string) {
 		return
 	}
 
+	// Parse Pagination Query Parameter
+	pageParam := r.URL.Query().Get("page")
+
+	pageNumber, err := strconv.Atoi(pageParam)
+	if err != nil || pageNumber < 1 {
+		log.Println("Invalid or missing page number")
+		http.Error(w, "Invalid or missing page number", http.StatusBadRequest)
+		return
+	}
 	// Process the resources and generate the HTML
 	html := ""
 	results := resources["results"].([]interface{})
 	for _, resource := range results {
 		resourceMap := resource.(map[string]interface{})
 		typeField := resourceMap["type"].(string)
-		html += "<tr><td>" + typeField + "</td></tr>"
+		html += `<tr 
+			hx-get="/resources/test?page=` + strconv.Itoa(pageNumber+1) + `&pageSize=2"
+			hx-trigger="revealed"
+			hx-swap="afterend">
+				<td>` +
+			typeField +
+			`</td>` +
+			`</tr>`
 	}
 
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
